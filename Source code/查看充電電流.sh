ff0b951cb06bb6af68ded37705cd826a6f2501b0
林博仁 <Buo.Ren.Lin@gmail.com>
2016-04-03 01:36:04 +0800

    Source code/查看充電電流.sh - 支援自動換算為常用單位
    Signed-off-by: 林博仁 <Buo.Ren.Lin@gmail.com>

--- Source code/查看充電電流.sh
+++ Source code/查看充電電流.sh
@@ -1,3 +1,3 @@
 #!/system/bin/sh
-busybox printf "目前的充電電流（帶負號）：`cat /sys/class/power_supply/battery/current_now` 微安培。\n"
+busybox printf "目前的充電電流：$((-($(cat /sys/class/power_supply/battery/current_now)) / 1000)) 毫安培。\n"
 exit 0
